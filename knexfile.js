9bd6ab9025852ba5237c31023627a9f85a765628
Will <allenwilliamdev@gmail.com>
2020-08-21 14:12:49 -0400

    [NOTICKET] - Add db dev and test config

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      user: 'postgres',
+      password: '1234',
+      database: 'allen_real_estates',
+    },
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      user: 'postgres',
+      password: '1234',
+      database: 'test_allen_real_estates',
+    },
+  }
+};
